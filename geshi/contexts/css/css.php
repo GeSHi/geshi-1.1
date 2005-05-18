d756b69853c38c2d186601a97ae5441ee854203b
Nigel McNie <nigel@catalyst.net.nz>
2005-05-18 11:53:13 +0000

    Added inline media context to CSS (begins with @media, has rules in it), to partially fix bug #2

--- geshi/contexts/css/css.php
+++ geshi/contexts/css/css.php
@@ -11,20 +11,25 @@
  * with GeSHi, in the docs/ directory.
  *
  * @package   lang
- * @author    Nigel McNie <oracle.shinoda@gmail.com>
+ * @author    Nigel McNie <nigel@geshi.org>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright (C) 2005 Nigel McNie
  * @version   $Id$
  * 
  */
 
+// If someone wanted to style the selectors, they would have to make
+// this context and inline_media into GeSHiCodeContexts, or make a new
+// context and make this and inline_media be overridden by it
+
 $this->_contextDelimiters = array();
 
 $this->_childContexts = array(
+    new GeSHiContext('css/inline_media'),
     new GeSHiCodeContext('css/rule', 'rule')
 );
 
-$this->_styler->setStyle($this->_styleName, 'color:#006;');
+$this->_styler->setStyle($this->_styleName, 'color:#000;');
 $this->_contextStyleType = GESHI_STYLE_NONE;
 // irrelevant if no delimiters...
 $this->_delimiterParseData = GESHI_CHILD_PARSE_BOTH;
