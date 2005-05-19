21f27db298c3e7fe7958308014901c10732d39b0
Nigel McNie <nigel@catalyst.net.nz>
2005-05-19 22:43:31 +0000

    Changed e-mail to be correct.

--- geshi/contexts/doxygen/doxygen.php
+++ geshi/contexts/doxygen/doxygen.php
@@ -11,7 +11,7 @@
  * with GeSHi, in the docs/ directory.
  *
  * @package   lang
- * @author    Nigel McNie <oracle.shinoda@gmail.com>
+ * @author    Nigel McNie <nigel@geshi.org>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright (C) 2005 Nigel McNie
  * @version   $Id$
