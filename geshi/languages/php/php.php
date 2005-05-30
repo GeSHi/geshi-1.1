12b5c1a7dd46665712304b62632ad7d06ae66e0e
Nigel McNie <nigel@catalyst.net.nz>
2005-05-30 23:53:29 +0000

    Went back to using the constructor-based infectious context load.

--- geshi/languages/php/php.php
+++ geshi/languages/php/php.php
@@ -25,7 +25,6 @@ require_once GESHI_CLASSES_ROOT . 'class.geshicodecontext.php';
  * PHP Language file for GeSHi
  */ 
 $this->_humanLanguageName = 'PHP';
-$this->_rootContext =& new GeSHiCodeContext('html/html', '', array());
-$this->_rootContext->addInfectiousContext(new GeSHiCodeContext('php'));
+$this->_rootContext =& new GeSHiCodeContext('html/html', '', array(), new GeSHiCodeContext('php'));
 
 ?>
