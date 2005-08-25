45f561af096a1d1f6584644366c02b875f7c8563
Nigel McNie <nigel@catalyst.net.nz>
2005-08-25 22:30:21 +0000

    Added vrml97 language.

---
+++ geshi/languages/web3d/vrml97.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * GeSHi - Generic Syntax Highlighter
+ * ----------------------------------
+ * 
+ * For information on how to use GeSHi, please consult the documentation
+ * found in the docs/ directory, or online at http://geshi.org/docs/
+ * 
+ *  This file is part of GeSHi.
+ *
+ *  GeSHi is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GeSHi is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GeSHi; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * You can view a copy of the GNU GPL in the COPYING file that comes
+ * with GeSHi, in the docs/ directory.
+ *
+ * @package   lang
+ * @author    Nigel McNie <nigel@geshi.org>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright (C) 2005 Nigel McNie
+ * @version   1.1.0alpha4
+ *
+ */
+
+/** Get the GeSHiCodeContext class */ 
+require_once GESHI_CLASSES_ROOT . 'class.geshicodecontext.php';
+
+/**
+ * VRML97 Language file for GeSHi
+ */ 
+$this->_humanLanguageName = 'VRML97';
+
+$this->_rootContext =& new GeSHiCodeContext('web3d', 'vrml97');
+
+?>
