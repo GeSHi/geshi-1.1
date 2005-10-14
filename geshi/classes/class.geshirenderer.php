d7d13f906a29a99074b1cf6fd17b5e7e7a5eb3ad
Nigel McNie <nigel@catalyst.net.nz>
2005-10-14 08:47:54 +0000

    Added abstract GeSHiRenderer class that can be extended by other classes to provide for different output formats, like PDF.

---
+++ geshi/classes/class.geshirenderer.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * GeSHi - Generic Syntax Highlighter
+ * 
+ * For information on how to use GeSHi, please consult the documentation
+ * found in the docs/ directory, or online at http://geshi.org/docs/
+ * 
+ *  This file is part of GeSHi.
+ *
+ *  GeSHi is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GeSHi is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GeSHi; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * You can view a copy of the GNU GPL in the COPYING file that comes
+ * with GeSHi, in the docs/ directory.
+ *
+ * @package   core
+ * @author    Nigel McNie <nigel@geshi.org>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright (C) 2005 Nigel McNie
+ * @version   $Id$
+ * 
+ */
+
+/**
+ * The GeSHiRenderer class
+ * 
+ * @package core
+ * @author  Nigel McNie <nigel@geshi.org>
+ * @since   1.1.1
+ * @version $Revision$
+ * @abstract
+ */
+class GeSHiRenderer {
+    
+    /**
+     * @var GeSHiStyler
+     * @access private
+     */
+    var $_styler;
+    
+    
+    function GeSHiRenderer(&$styler)
+    {
+        $this->_styler =& $styler;
+    }
+    
+    /**
+     * Abstract. Renderers should implement this method to
+     * get access to parse tokens
+     */
+    function parseToken ($token, $context_name, $url) {}
+    
+    /**
+     * Should return any header data for the renderer
+     */
+    function getHeader () {}
+    
+    /**
+     * Should return any footer data for the renderer
+     */
+    function getFooter () {}
+}
+
+?>
