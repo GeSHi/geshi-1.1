983e6b479a49fe4eb8ee77ab24ba374176b8c5ba
Benny Baumann <benbe@geshi.org>
2006-02-14 23:12:22 +0000

    fix: Modified the theme files to better suite the Borland Delphi 9 and 10 IDE defaults, that got heavily changed in those versions

--- geshi/themes/boride/d10/delphi/delphi.php
+++ geshi/themes/boride/d10/delphi/delphi.php
@@ -35,22 +35,58 @@
 /*
  * Delphi styles for boring Borland IDE theme
  */
-$this->setStyle('single_string', 'color:#000;');
-$this->setStyle('single_string/esc', 'color:#000;');
+$this->setStyle('multi_comment', 'color:#008000;font-style:italic;');
+$this->setStyle('single_comment', 'color:#008000;font-style:italic;');
+$this->setStyle('single_string', 'color:#0000FF;');
+$this->setStyle('single_string/esc', 'color:#0000FF;');
 
-$this->setStyle('multi_comment', 'color:#000080;font-style:italic;');
-$this->setStyle('single_comment', 'color:#000080;font-style:italic;');
-$this->setStyle('preprocessor', 'color:#008000;font-style:italic;');
-
-$this->setStyle('keyword', 'color:#000;font-weight:bold;');
+$this->setStyle('preprocessor', 'color:#008080;font-style:italic;');
 
 $this->setStyle('asm', 'color:#000;');
-$this->setStyle('asm/start', 'color:#000;font-weight:bold;');
-$this->setStyle('asm/end', 'color:#000;font-weight:bold;');
-$this->setStyle('asm/keyword', 'color:#000;font-weight:bold;');
-$this->setStyle('asm/keyop', 'color:#000;font-weight:bold;');
-$this->setStyle('asm/control', 'color:#000;font-weight:bold;');
+$this->setStyle('asm/start', 'color:#000080;font-weight:bold;');
+$this->setStyle('asm/end', 'color:#000080;font-weight:bold;');
+$this->setStyle('asm/keyword', 'color:#000080;font-weight:bold;');
+$this->setStyle('asm/keyop', 'color:#000080;font-weight:bold;');
+$this->setStyle('asm/control', 'color:#000;font-weight: bold;');
 $this->setStyle('asm/register', 'color:#000;');
-$this->setStyle('asm/instr/*', 'color:#000;font-weight:bold;');
+$this->setStyle('asm/instr/i386', 'color:#000;font-weight:bold;');
+$this->setStyle('asm/instr/i387', 'color:#000;font-weight:bold;');
+$this->setStyle('asm/instr/mmx', 'color:#000;font-weight:bold;');
+$this->setStyle('asm/instr/sse', 'color:#000;font-weight:bold;');
+$this->setStyle('asm/instr/3Dnow', 'color:#000;font-weight:bold;');
+$this->setStyle('asm/instr/3Dnow2', 'color:#000;font-weight:bold;');
+
+$this->setStyle('asm/symbol', 'color:#000000;');
+$this->setStyle('asm/label', 'color:#000;');
+$this->setStyle('asm/num/int', 'color:#000;');
+$this->setStyle('asm/num/dbl', 'color:#000;');
+$this->setStyle('asm/hex', 'color: #000;');
+
+$this->setStyle('asm/oodynamic', 'color:#000;');
+
+$this->setStyle('keyword', 'color:#000080;font-weight:bold;');
+$this->setStyle('keytype', 'color:#000;font-weight:bold;');
+$this->setStyle('keyident', 'color:#000;font-weight:bold;');
+
+$this->setStyle('symbol', 'color:#000000;');
+$this->setStyle('ctrlsym', 'color:#000000;');
+$this->setStyle('oopsym', 'color:#008000;');
+$this->setStyle('brksym', 'color:#000000;');
+$this->setStyle('mathsym', 'color:#000000;');
+$this->setStyle('cmpsym', 'color:#000000;');
+
+$this->setStyle('char', 'color:#0000FF;');
+$this->setStyle('charhex', 'color:#0000FF;');
+$this->setStyle('hex', 'color: #0000FF;');
+
+$this->setStyle('oodynamic', 'color:#559;');
+
+$this->setStyle('stdprocs/system', 'color:#444;');
+$this->setStyle('stdprocs/sysutil', 'color:#444;');
+$this->setStyle('stdprocs/class', 'color:#444;');
+$this->setStyle('stdprocs/math', 'color:#444;');
+
+$this->setStyle('num/int', 'color:#0000FF;');
+$this->setStyle('num/dbl', 'color:#0000FF;');
 
 ?>
