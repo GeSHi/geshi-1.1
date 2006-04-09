e36edbcaf1713c7af77688b7a11f73fb1f60ace9
Nigel McNie <nigel@catalyst.net.nz>
2006-04-09 08:58:45 +0000

    Changed HTML language to new format.

--- geshi/languages/html/html.php
+++ geshi/languages/html/html.php
@@ -32,21 +32,82 @@
  *
  */
 
-/*
- * HTML language file for GeSHi
- * 
- * [notes about language]
- * 
- * [notes about this implementation of the language]
- * 
- */
+function geshi_html_html (&$context)
+{
+    $context->addChild('doctype');
+    $context->addChild('tag', 'code');
+    $context->addChild('comment');
+    //@todo [blocking 1.1.9] The <![CDATA[ was added to stop CSS jumping into attribute selector context
+    //the moment it was encountered, but this only really applies to XML
+    $context->addChildLanguage('css/css', 'REGEX#<style[^>]+>\s*(<!\[CDATA\[)?#i', '</style>',
+        false, GESHI_CHILD_PARSE_NONE);
+    $context->addChildLanguage('javascript/javascript', 'REGEX#<script[^>]+>#i', '</script>',
+        false, GESHI_CHILD_PARSE_NONE);
 
-/** Get the GeSHiCodeContext class */ 
-require_once GESHI_CLASSES_ROOT . 'class.geshicodecontext.php';
+    $context->addRegexGroup('#(&(([a-z0-9]{2,5})|(\#[0-9]{2,4}));)#', '&',
+        array(
+            1 => array('entity', false)
+        )
+    );
+}
 
+function geshi_html_html_doctype (&$context)
+{
+    $context->addDelimiters('<!DOCTYPE ', '>');
+    $context->addChild('string');
+}
 
-$this->_humanLanguageName = 'HTML';
+function geshi_html_html_tag (&$context)
+{
+    $context->addDelimiters('REGEX#<[/a-z_0-6]+#i', '>');
+    $context->addChild('string');
 
-$this->_rootContext =& new GeSHiCodeContext('html');
+    // Attributes
+    $context->addKeywordGroup(array(
+            'abbr', 'accept-charset', 'accept', 'accesskey', 'action', 'align',
+            'alink', 'alt', 'archive', 'axis', 'background', 'bgcolor', 'border',
+            'cellpadding', 'cellspacing', 'char', 'charoff', 'charset', 'checked',
+            'cite', 'class', 'classid', 'clear', 'code', 'codebase', 'codetype',
+            'color', 'cols', 'colspan', 'compact', 'content', 'coords', 'data',
+            'datetime', 'declare', 'defer', 'dir', 'disabled', 'enctype', 'face',
+            'for', 'frame', 'frameborder', 'headers', 'height', 'href', 'hreflang',
+            'hspace', 'http-equiv', 'id', 'ismap', 'label', 'lang', 'language',
+            'link', 'longdesc', 'marginheight', 'marginwidth', 'maxlength', 'media',
+            'method', 'multiple', 'name', 'nohref', 'noresize', 'noshade', 'nowrap',
+            'object', 'onblur', 'onchange', 'onclick', 'ondblclick', 'onfocus',
+            'onkeydown', 'onkeypress', 'onkeyup', 'onload', 'onmousedown',
+            'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onreset',
+            'onselect', 'onsubmit', 'onunload', 'profile', 'prompt', 'readonly',
+            'rel', 'rev', 'rows', 'rowspan', 'rules', 'scheme', 'scope', 'scrolling',
+            'selected', 'shape', 'size', 'span', 'src', 'standby', 'start', 'style',
+            'summary', 'tabindex', 'target', 'text', 'title', 'type', 'usemap',
+            'valign', 'value', 'valuetype', 'version', 'vlink', 'vspace', 'width'
+        ), 'attribute'
+    );
+    
+    $context->addSymbolGroup(array('=', '/'), 'symbol');
+}
+
+function geshi_html_html_tag_string (&$context)
+{
+    $context->addDelimiters("'", "'");
+    $context->addDelimiters('"', '"');
+    $context->alias('string');
+    // NOTE: need to support parsing left delimiter only and _neverTrim
+    $context->addChildLanguage('javascript/javascript', array('javascript:', 'return'), array('"', "'"),
+        false, GESHI_CHILD_PARSE_LEFT);
+    //$this->_contextStyleType = GESHI_STYLE_STRINGS;
+}
+
+function geshi_html_html_doctype_string (&$context)
+{
+    geshi_html_html_tag_string($context);
+}
+
+function geshi_html_html_comment (&$context)
+{
+    $context->addDelimiters('<!--', '-->');
+    //$this->_contextStyleType = GESHI_STYLE_COMMENTS;
+}
 
 ?>
