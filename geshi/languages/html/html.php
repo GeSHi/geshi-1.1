681c1738c3cf1b6f0c483ce777c7eeba76d018f3
Nigel McNie <nigel@catalyst.net.nz>
2006-04-26 08:20:13 +0000

    New header format. Minor style changes.

--- geshi/languages/html/html.php
+++ geshi/languages/html/html.php
@@ -1,34 +1,37 @@
 <?php
 /**
  * GeSHi - Generic Syntax Highlighter
+ * <pre>
+ *   File:   geshi/languages/html/html.php
+ *   Author: Nigel McNie
+ *   E-mail: nigel@geshi.org
+ * </pre>
  * 
  * For information on how to use GeSHi, please consult the documentation
  * found in the docs/ directory, or online at http://geshi.org/docs/
  * 
- *  This file is part of GeSHi.
+ * This program is part of GeSHi.
  *
- *  GeSHi is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  GeSHi is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
+ * 
  *  You should have received a copy of the GNU General Public License
- *  along with GeSHi; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  *
- * You can view a copy of the GNU GPL in the COPYING file that comes
- * with GeSHi, in the docs/ directory.
- *
- * @package   lang
- * @author    Nigel McNie <nigel@geshi.org>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright (C) 2005 Nigel McNie
- * @version   $Id$
+ * @package    geshi
+ * @subpackage core
+ * @author     Nigel McNie <nigel@geshi.org>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2004 - 2006 Nigel McNie
+ * @version    $Id$
  *
  */
 
@@ -44,8 +47,7 @@ function geshi_html_html (&$context)
     $context->addChildLanguage('javascript/javascript', 'REGEX#<script[^>]+>#i', '</script>',
         false, GESHI_CHILD_PARSE_NONE);
 
-    $context->addRegexGroup('#(&(([a-z0-9]{2,5})|(\#[0-9]{2,4}));)#', '&',
-        array(
+    $context->addRegexGroup('#(&(([a-z0-9]{2,5})|(\#[0-9]{2,4}));)#', '&', array(
             1 => array('entity', false)
         )
     );
@@ -54,13 +56,13 @@ function geshi_html_html (&$context)
 function geshi_html_html_doctype (&$context)
 {
     $context->addDelimiters('<!DOCTYPE ', '>');
-    $context->addChild('string');
+    $context->addChild('html/html/string');
 }
 
 function geshi_html_html_tag (&$context)
 {
     $context->addDelimiters('REGEX#<[/a-z_0-6]+#i', '>');
-    $context->addChild('string');
+    $context->addChild('html/html/string');
 
     // Attributes
     $context->addKeywordGroup(array(
@@ -88,20 +90,14 @@ function geshi_html_html_tag (&$context)
     $context->addSymbolGroup(array('=', '/'), 'symbol');
 }
 
-function geshi_html_html_tag_string (&$context)
+function geshi_html_html_string (&$context)
 {
     $context->addDelimiters("'", "'");
     $context->addDelimiters('"', '"');
-    $context->alias('string');
-    // NOTE: need to support parsing left delimiter only and _neverTrim
+    // NOTE: need to support _neverTrim
     $context->addChildLanguage('javascript/javascript', array('javascript:', 'return'), array('"', "'"),
         false, GESHI_CHILD_PARSE_LEFT);
     //$this->_contextStyleType = GESHI_STYLE_STRINGS;
-}
-
-function geshi_html_html_doctype_string (&$context)
-{
-    geshi_html_html_tag_string($context);
 }
 
 function geshi_html_html_comment (&$context)
