6346a9391b5f0e5c9edf80575e798e83e67d00a3
Nigel McNie <nigel@catalyst.net.nz>
2006-04-26 08:10:24 +0000

    Header comment changes. Added some missing API methods.

--- geshi/classes/class.geshistringcontext.php
+++ geshi/classes/class.geshistringcontext.php
@@ -1,34 +1,37 @@
 <?php
 /**
  * GeSHi - Generic Syntax Highlighter
+ * <pre>
+ *   File:   geshi/classes/class.geshistringcontext.php
+ *   Author: Nigel McNie
+ *   E-mail: nigel@geshi.org
+ * </pre>
  * 
  * For information on how to use GeSHi, please consult the documentation
  * found in the docs/ directory, or online at http://geshi.org/docs/
  * 
- *  This file is part of GeSHi.
+ * This program is part of GeSHi.
  *
- *  GeSHi is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  GeSHi is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
+ * 
  *  You should have received a copy of the GNU General Public License
- *  along with GeSHi; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  *
- * You can view a copy of the GNU GPL in the COPYING file that comes
- * with GeSHi, in the docs/ directory.
- *
- * @package   core
- * @author    Nigel McNie <nigel@geshi.org>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright (C) 2005 Nigel McNie
- * @version   $Id$
+ * @package    geshi
+ * @subpackage core
+ * @author     Nigel McNie <nigel@geshi.org>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2004 - 2006 Nigel McNie
+ * @version    $Id$
  * 
  */
 
@@ -36,11 +39,12 @@
  * The GeSHiStringContext class. This class extends GeSHiContext to handle
  * the concept of escape characters that strings often use.
  *  
- * @package core
- * @author  Nigel McNie <nigel@geshi.org>
- * @since   1.1.0
- * @version $Revision$
- * @see     GeSHiContext
+ * @package    geshi
+ * @subpackage core
+ * @author     Nigel McNie <nigel@geshi.org>
+ * @since      1.1.0
+ * @version    $Revision$
+ * @see        GeSHiContext
  */
 class GeSHiStringContext extends GeSHiContext
 {
@@ -60,6 +64,23 @@ class GeSHiStringContext extends GeSHiContext
     var $_lastOpener;
     
     /**#@-*/
+    
+    // {{{ setEscapeCharacters()
+    
+    function setEscapeCharacters ($chars)
+    {
+        $this->_escapeCharacters = (array) $chars;
+    }
+    
+    // }}}
+    // {{{ setCharactersToEscape()
+    
+    function setCharactersToEscape ($chars)
+    {
+        $this->_charsToEscape = (array) $chars;
+    }
+    
+    // }}}
     
     /**
      * GetContextEndData
