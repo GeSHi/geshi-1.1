1b917ab3893f3974f9cf4225e564e8ecbdd89664
Nigel McNie <nigel@catalyst.net.nz>
2006-04-29 22:51:06 +0000

    Changed the header comment to the new format, and commented the geshi_styler function.

--- geshi/functions.geshi.php
+++ geshi/functions.geshi.php
@@ -1,34 +1,37 @@
 <?php
 /**
  * GeSHi - Generic Syntax Highlighter
+ * <pre>
+ *   File:   geshi/functions.geshi.php
+ *   Author: Nigel McNie
+ *   E-mail: nigel@geshi.org
+ * </pre>
  * 
  * For information on how to use GeSHi, please consult the documentation
  * found in the docs/ directory, or online at http://geshi.org/docs/
  * 
- *  This file is part of GeSHi.
+ * This program is part of GeSHi.
  *
- *  GeSHi is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  GeSHi is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
+ * 
  *  You should have received a copy of the GNU General Public License
- *  along with GeSHi; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  *
- * You can view a copy of the GNU GPL in the COPYING file that comes
- * with GeSHi, in the docs/ directory.
- *
- * @package   core
- * @author    Nigel McNie <nigel@geshi.org>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright (C) 2005 Nigel McNie
- * @version   $Id$
+ * @package    geshi
+ * @subpackage core
+ * @author     Nigel McNie <nigel@geshi.org>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2004 - 2006 Nigel McNie
+ * @version    $Id$
  * 
  */
 
@@ -128,7 +131,8 @@ function geshi_can_include ($file_name)
     }
 
     // Check if we need to check for symlinks and if all required functions are available
-    // The availability check is due to compatibility to M$ Windows as PHP doesn't implement some symlink functions there.
+    // The availability check is due to compatibility to M$ Windows as PHP doesn't implement
+    // some symlink functions there.
     $can_include = true;
     if (!GESHI_ALLOW_SYMLINK_PATHS && function_exists('is_link')) {
         do {
@@ -250,7 +254,13 @@ if (!function_exists('stripos')) {
 	}
 }
 
-
+/**
+ * Returns the GeSHi_Styler object used to help with parsing
+ * 
+ * @param boolean $force_new If true, forces the creation of
+ *                           a new GeSHi_Parser object
+ * @return GeSHi_Styler
+ */
 function &geshi_styler ($force_new = false)
 {
     static $styler = null;
