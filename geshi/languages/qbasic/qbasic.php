cbe9bf4e8b482cc015dfc9a5f82c96b24f6d94f0
Nigel McNie <nigel@catalyst.net.nz>
2006-05-21 06:46:32 +0000

    Converted QBasic language to the new format, and added some keywords.

--- geshi/languages/qbasic/qbasic.php
+++ geshi/languages/qbasic/qbasic.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * GeSHi - Generic Syntax Highlighter
+ * <pre>
+ *   File:   geshi/languages/qbasic/qbasic.php
+ *   Author: Nigel McNie
+ *   E-mail: nigel@geshi.org
+ * </pre>
  * 
  * For information on how to use GeSHi, please consult the documentation
  * found in the docs/ directory, or online at http://geshi.org/docs/
@@ -24,29 +29,86 @@
  * You can view a copy of the GNU GPL in the COPYING file that comes
  * with GeSHi, in the docs/ directory.
  *
- * @package   lang
- * @author    Nigel McNie <nigel@geshi.org>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright (C) 2005 Nigel McNie
- * @version   $Id$
+ * @package    geshi
+ * @subpackage lang
+ * @author     Nigel McNie <nigel@geshi.org>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2004 - 2006 Nigel McNie
+ * @version    $Id$
  *
  */
 
-/*
- * QBasic language file for GeSHi
- * 
- * [notes about language]
- * 
- * [notes about this implementation of the language]
- * 
- */
+function geshi_qbasic_qbasic (&$context)
+{
+    $context->addChild('string');
+    $context->addChild('comment');
+    
+    $context->addKeywordGroup(array(
+        'case', 'case else', 'do', 'else', 'elseif', 'exit for', 'exit do', 'for', 'if',
+        'loop', 'next', 'step', 'select case', 'then', 'to', 'until', 'wend', 'while',
+        
+        'dim', 'data'
+    ), 'keyword', false, 'geshi_qbasic_url()');
 
-/** Get the GeSHiCodeContext class */ 
-require_once GESHI_CLASSES_ROOT . 'class.geshicodecontext.php';
+    $context->addKeywordGroup(array(
+        'function', 'end function', 'sub', 'end sub', 'call', 'exit function', 'exit sub',
+        'declare', 'common', 'shared', 'static', 'def fn', 'end def', 'exit def', 'gosub',
+        'chain', 'return',
+        // Hmm?
+        'calls', 'call absolute', 'call interrupt', 'run'
+    ), 'procedure', false, 'geshi_qbasic_url()');
+    
+    $context->addKeywordGroup(array(
+        'print', 'print using', 'width', 'inkey$', 'input$', 'input', 'line input',
+        'locate', 'spc', 'tab', 'csrlin', 'pos', 'view print',
+        // file IO
+        'open', 'close', 'write', 'put', 'get', 'files', 'freefile', 'kill', 'name',
+        'eof', 'fileattr', 'loc', 'lof', 'seek', 'read'
+        // file IO dupes
+        // 'print', 'print using', 'width', 'input', 'input$', 'line input' 
+    ), 'io', false, 'geshi_qbasic_url()');
+    
+    $context->addKeywordGroup(array(
+        'left$', 'right$', 'ltrim$', 'rtrim$', 'mid$', 'instr', 'lcase$', 'ucase$', 'lset',
+        'rset', 'str$', 'val', 'cvi', 'cvs', 'cvl', 'cvd', 'cvsmbf', 'cvdmbf', 'mki$', 'mks$',
+        'mkl$', 'mkd$', 'mksmbf$', 'mkdmbf$', 'space$', 'string$', 'len', 'asc', 'chr$'
+    ), 'string', false, 'geshi_qbasic_url()');
+    
+    $context->addKeywordGroup(array(
+        'screen', 'pset', 'preset', 'line', 'circle', 'draw', 'view', 'window', 'pmap',
+        'point', 'color', 'palette', 'paint', 'pcopy', 'cls'
+        // get, put also here but in IO as well...
+    ), 'draw', false, 'geshi_qbasic_url()');
+    
+    $context->addKeywordGroup(array(
+        'on error goto', 'resume', 'err', 'erl', 'erdev', 'erdev$', 'error', 'on', 'off',
+        'stop'
+    ), 'error', false, 'geshi_qbasic_url()');
+    
+    $context->addSymbolGroup(array(
+        '(', ')', ',', ':', ';', '=', '<', '>', '+', '*', '/', '-'
+    ), 'symbol');
+    
+    $context->useStandardIntegers();
+    $context->useStandardDoubles();
+}
 
+function geshi_qbasic_qbasic_string (&$context)
+{
+    $context->addDelimiters('"', '"');
+    //$this->_contextStyleType = GESHI_STYLE_STRINGS;
+}
 
-$this->_humanLanguageName = 'QBasic';
+function geshi_qbasic_qbasic_comment (&$context)
+{
+    $context->addDelimiters(array("'", 'REM'), "\n");
+    //$this->_contextStyleType = GESHI_STYLE_COMMENTS;
+    $context->parseDelimiters(GESHI_CHILD_PARSE_LEFT);
+}
 
-$this->_rootContext =& new GeSHiCodeContext('qbasic');
+function geshi_qbasic_url ($keyword)
+{
+    return 'http://qboho.qbasicnews.com/qboho/qck' . strtolower($keyword) . '.html';
+}
 
 ?>
