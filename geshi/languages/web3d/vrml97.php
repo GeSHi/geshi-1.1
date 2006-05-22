cc9def1ddb0ff86f0bd941dad46b6e81f61f31d1
Nigel McNie <nigel@catalyst.net.nz>
2006-05-22 09:16:37 +0000

    Converted web3d's vrml97 language to the new language file format.

--- geshi/languages/web3d/vrml97.php
+++ geshi/languages/web3d/vrml97.php
@@ -33,14 +33,159 @@
  *
  */
 
-/** Get the GeSHiCodeContext class */ 
-require_once GESHI_CLASSES_ROOT . 'class.geshicodecontext.php';
+function geshi_web3d_vrml97 (&$context)
+{
+    $context->addChild('comment');
+    $context->addChild('string');
+    
+    // Nodes
+    $context->addKeywordGroup(array(
+        // sensors
+        'CylinderSensor', 'PlaneSensor', 'ProximitySensor', 'SphereSensor',  
+        'TimeSensor', 'TouchSensor', 'VisibilitySensor',
+        // interpolators 
+        'ColorInterpolator', 'CoordinateInterpolator', 'NormalInterpolator',
+        'OrientationInterpolator', 'PositionInterpolator', 'ScalarInterpolator',
+        // grouping nodes
+        'Anchor', 'Billboard', 'Collision', 'Group', 'LOD', 'Switch', 'Transform',
+        // bindables
+        'Background', 'Fog', 'NavigationInfo', 'Viewpoint',
+        // lights
+        'DirectionalLight', 'PointLight', 'SpotLight',
+        // shape and geometry 
+        'Box', 'Cone', 'Coordinate', 'Cylinder', 'ElevationGrid', 'Extrusion',
+        'IndexedFaceSet', 'IndexedLineSet', 'PointSet', 'Shape', 'Sphere', 'Text', 
+        // appearance 
+        'Appearance', 'Color', 'FontStyle', 'Material', 
+        'Normal', 'TextureCoordinate', 'TextureTransform',
+        // textures 
+        'ImageTexture', 'MovieTexture', 'PixelTexture', 
+        // sound
+        'AudioClip', 'Sound', 
+        // other
+        'Inline', 'Script', 'WorldInfo'
+    ), 'node', true, 'http://www.web3d.org/x3d/specifications/vrml/ISO-IEC-14772-IS-VRML97'
+        . 'WithAmendment1/part1/nodesRef.html#{FNAME}');
 
-/**
- * VRML97 Language file for GeSHi
- */ 
-$this->_humanLanguageName = 'VRML97';
+    // Fields of nodes
+    $context->addKeywordGroup(array(
+        // A
+        'addChildren', 'ambientIntensity', 'appearance', 'attenuation', 'autoOffset', 
+        'avatarSize', 'axisOfRotation',
+        // B 
+        'backUrl', 'bboxCenter', 'bboxSize', 'beamWidth', 'beginCap', 
+        'bindTime', 'bottom', 'bottomRadius', 'bottomUrl',
+        // C
+        'ccw', 'center', 'children', 'choice', 'collide', 
+        'collideTime', 'color', 'colorIndex', 'colorPerVertex', 'convex', 
+        'coord', 'coordIndex', 'creaseAngle', 'crossSection', 'cutOffAngle', 
+        'cycleInterval', 'cycleTime', 
+        // D
+        'description', 'diffuseColor', 'direction', 'directOutput', 
+        'diskAngle', 'duration_changed', 
+        // E
+        'emissiveColor', 'enabled', 'endCap', 'enterTime', 'exitTime', 
+        // F
+        'family', 'fieldOfView', 'fogType', 'fontStyle', 'fraction_changed', 
+        'frontUrl', 
+        // G
+        'geometry', 'groundAngle', 'groundColor',
+        // H 
+        'headlight', 'height', 'hitNormal_changed', 'hitPoint_changed', 'horizontal',
+        // I
+        'image', 'info', 'intensity', 'isActive', 'isBound', 'isOver',
+        // J
+        'jump', 'justify', 
+        // K
+        'key', 'keyValue', 
+        // L  
+        'language', 'leftUrl', 'leftToRight','length', 'level', 
+        'location', 'loop', 
+        // M
+        'material', 'maxAngle', 'maxBack', 'maxExtent', 'maxFront', 
+        'maxPosition', 'minAngle', 'minBack', 'minFront', 'minPosition', 
+        'mustEvaluate', 
+        // N
+        'normal', 'normalIndex', 'normalPerVertex', 
+        // O
+        'offset', 'on', 'orientation', 'orientation_changed', 
+        // P
+        'parameter', 'pitch', 'point', 'position', 'position_changed', 
+        'priority', 'proxy', 
+        // Q
+        
+        // R
+        'radius', 'range', 'removeChildren', 'repeatS', 'repeatT', 
+        'rightUrl', 'rotation', 'rotation_changed',
+        // S
+        'scale', 'scaleOrientation', 'set_bind', 'set_colorIndex', 'set_coordIndex', 
+        'set_crossSection', 'set_fraction', 'set_height', 'set_normalIndex', 'set_orientation', 
+        'set_spine', 'set_scale', 'set_texCoordIndex', 'shininess', 'side', 
+        'size', 'skyAngle', 'skyColor', 'solid', 'source', 
+        'spacing', 'spatialization', 'specularColor', 'speed', 'spine', 
+        'startTime', 'stopTime', 'string', 'style', 
+        // T
+        'texCoord', 'texCoordIndex', 'texture', 'textureTransform', 'time', 
+        'title', 'top', 'topUrl', 'topToBottom', 'touchTime', 
+        'trackPoint_changed', 'translation', 'translation_changed', 'transparency', 'type',
+        // U
+        'url', 
+        // V
+        'value_changed', 'vector', 'visibilityLimit', 'visibilityRange', 
+        // W
+        'whichChoice', 
+        // X
+        'xDimension', 'xSpacing', 
+        // Y
+        
+        // Z
+        'zDimension', 'zSpacing' 
+    ), 'field', true);
+    
+    // Keywords
+    $context->addKeywordGroup(array(
+        'DEF', 'USE', 'IS', 'PROTO', 'EXTERNPROTO', 'TO', 'ROUTE',
+        'TRUE', 'FALSE', 'NULL',
+    ), 'keyword', true);
+    
+    // Field access types
+    $context->addKeywordGroup(array(
+        'eventIn', 'eventOut', 'exposedField', 'field', 
+    ), 'fieldaccess', true);
+    
+    // Field data types
+    $context->addKeywordGroup(array(
+        'SFBool', 'SFColor', 'SFFloat', 'SFImage', 'SFInt32', 'SFNode',
+        'SFRotation', 'SFString', 'SFTime', 'SFVec2f', 'SFVec3f',
+        'MFColor', 'MFFloat', 'MFInt32', 'MFNode',
+        'MFRotation', 'MFString', 'MFTime', 'MFVec2f', 'MFVec3f',
+    ), 'fieldtype', true);
 
-$this->_rootContext =& new GeSHiCodeContext('web3d', 'vrml97');
+    $context->addSymbolGroup(array(
+        '{', '}'
+    ), 'nodesymbol');
+    
+    $context->addSymbolGroup(array(
+        '[', ']'
+    ), 'arraysymbol');
+
+    $context->useStandardIntegers();
+    $context->useStandardDoubles();
+}
+
+function geshi_web3d_vrml97_comment (&$context)
+{
+    $context->addDelimiters('#', "\n");
+    $context->parseDelimiters(GESHI_CHILD_PARSE_LEFT);
+    //$this->_contextStyleType = GESHI_STYLE_COMMENTS;
+}
+
+function geshi_web3d_vrml97_string (&$context)
+{
+    $context->addDelimiters('"', '"');
+    $context->addChildLanguage('javascript/javascript',
+        array('REGEX#javascript:#', 'REGEX#ecmascript:#', 'REGEX#vrmlscript:#'), '"',
+        false, GESHI_CHILD_PARSE_LEFT);
+}
 
 ?>
