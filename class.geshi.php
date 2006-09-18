9373070f6e091dc6b5d73d1f898069875f96452e
Nigel McNie <nigel@catalyst.net.nz>
2006-09-18 12:42:21 +0000

    Bumped version for 1.1.2a4 development.

--- class.geshi.php
+++ class.geshi.php
@@ -42,7 +42,7 @@
 $geshi_old_reporting_level = error_reporting(E_ALL);
 
 /** GeSHi Version */
-define('GESHI_VERSION', '1.1.2alpha3');
+define('GESHI_VERSION', '1.1.2alpha4dev');
 
 /** Set the correct directory separator */
 define('GESHI_DIR_SEP', ('WIN' != substr(PHP_OS, 0, 3)) ? '/' : '\\');
